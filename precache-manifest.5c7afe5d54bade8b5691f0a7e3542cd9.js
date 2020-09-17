92b6ab052ca88f82bd1a05cdbe734906e5cb22b7
Vivek-396 <vivekkumarkanpuriit@gmail.com>
2020-09-17 17:41:55 +0530

    Updates

---
+++ precache-manifest.5c7afe5d54bade8b5691f0a7e3542cd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4cf26f7a2b3099d95fe5a785ffdd327",
+    "url": "/Moster-Rolodex/index.html"
+  },
+  {
+    "revision": "2fd7708df2ef218bffaf",
+    "url": "/Moster-Rolodex/static/css/main.5b273c48.chunk.css"
+  },
+  {
+    "revision": "e825732d0bd05873e6fa",
+    "url": "/Moster-Rolodex/static/js/2.a797de9e.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/Moster-Rolodex/static/js/2.a797de9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fd7708df2ef218bffaf",
+    "url": "/Moster-Rolodex/static/js/main.c302e74b.chunk.js"
+  },
+  {
+    "revision": "24f94c882f5d2e2357ce",
+    "url": "/Moster-Rolodex/static/js/runtime-main.cf1c05b4.js"
+  }
+]);
